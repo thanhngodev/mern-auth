78725950d45544db8f087e6ea65df8e692950ca9
thanhngodev <thanhngodev@gmail.com>
2024-11-03 23:30:20 +0700

    handle add login and logout

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,14 +1,14 @@
 import express from "express";
-import { login, logout, signup, verifyEmail } from "../controllers/auth.controller.js";
+import { signIn, signOut, signup, verifyEmail } from "../controllers/auth.controller.js";
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.send("Hello Auth Service!!!");
 });
 
-router.post("/signup", signup);
-router.post("/login", login);
-router.post("/logout", logout);
+router.post("/sign-up", signup);
+router.post("/sign-in", signIn);
+router.post("/sign-out", signOut);
 
 router.post("/verify-email", verifyEmail);
 
